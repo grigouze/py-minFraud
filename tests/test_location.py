1836720a818f58e3d2743bd46cc8a2a0572d9ab4
Grigouze <grigouze@yahoo.fr>
2011-03-27 18:08:05 +0200

    add test with real infos

--- tests/test_location.py
+++ tests/test_location.py
@@ -8,6 +8,13 @@ from minfraud.location import Location
 
 if __name__ == '__main__':
     aa = Location()
-    aa.input( { 'phone': 'New York' } )
+    input = {
+        'i': '24.24.24.24',
+        'city': 'NewYork',
+        'region': 'NY',
+        'postal': '10011',
+        'country': 'US',
+    }
+    aa.input( input )
     aa.query()
     print aa.output()
